39f2d14bc7d8744054c22fb45c30efdc241030a8
Eduardo Lacerda <dudu.lacerda.dl@gmail.com>
2020-05-03 17:27:25 -0300

    conclusão do curso de JS

--- Curso JS/ex018/script.js
+++ Curso JS/ex018/script.js
@@ -1,22 +1,66 @@
-var memoria = ['']
+var valores = []
+var lista = document.querySelector('.flista')
+var num = document.querySelector('.fnum')
+var res = document.querySelector('.res')
 
-function adicionar () {
-    var tab = document.querySelector('.selnum')
-    var num = document.querySelector('.txtnum')
-
-    if (num.value.length == '' ) {
-        window.alert('[ERRO]Digite um número!')
+function inNumero(n){
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
     } else {
-        var n = Number(num.value)
-        var item = document.createElement('option')
-        item.text = (`O valor ${n} foi adicionado`)
-        tab.appendChild(item)
+        return false
     }
 }
 
-memoria.push(n)
+function inLista(n, l){
+    if (l.indexOf(Number(n)) != -1){
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar () {
+    if (inNumero(num.value) && !inLista(num.value, valores)){
+        valores.push(Number(num.value))
+        var item = document.createElement('option')
+        item.text = `O valor ${num.value} foi adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
+    } else if (num.value.length == '' ) {
+        window.alert('[ERRO]Digite um número!')
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
+    }
+    num.value = ''
+    num.focus()
+}
 
 function finalizar () {
-    var texto = document.createElement('p')
-    texto
+    if (valores.length == 0){
+        window.alert ('Adicione valores antes de finalizar!')
+    } else {
+        var total = valores.length
+        var maior = valores [0]
+        var menor = valores [0]
+        var soma = 0
+        var media = 0
+        for (var pos in valores){
+            soma += valores[pos]
+
+            if (valores[pos] > maior){
+                maior = valores[pos]
+            }
+            if (valores[pos] < menor){
+                menor = valores[pos]
+            }
+        }
+        var media = soma / total
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${total} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior número informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor número informado foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${media}.</p>`
+    }
 }
