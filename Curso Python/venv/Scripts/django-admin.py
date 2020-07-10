2d01a1d10a767338018ee13bf8d11ea492ee5bdf
Eduardo Lacerda <dudu.lacerda.dl@gmail.com>
2020-07-10 14:44:12 -0300

    Atualização

--- Curso Python/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!D:\dudu_\Documents\GitHub\prototipos\Curso Python\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
